3976ee0eb84c6c3c8605b1aa0ed64b6ba86b96f3
Liam Roche <roche.liamp@gmail.com>
2022-04-09 14:12:39 -0400

    hid django secret key

--- backend_rest_api/backend_rest_api/settings.py
+++ backend_rest_api/backend_rest_api/settings.py
@@ -19,8 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
+from decouple import config
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-76t0$3ku*710n+k82(xdi$t%$8!6xg0naj#a$)j*qwpc0*iv$9'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
