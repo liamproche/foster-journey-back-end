09068a3dfe461b1f99d15171c41c80768ce9ddb8
Liam Roche <roche.liamp@gmail.com>
2022-04-09 12:25:17 -0400

    setup model

--- backend_rest_api/backend_rest_api/settings.py
+++ backend_rest_api/backend_rest_api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'placements_api',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,9 +75,16 @@ WSGI_APPLICATION = 'backend_rest_api.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+        'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'foster_journey',
+        'USER': 'postgres',
+        'PASSWORD': 'v052xd4',
+        'HOST': 'localhost'
     }
 }
 
--- backend_rest_api/placements_api/models.py
+++ backend_rest_api/placements_api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class Placement(models.Model):
+    num = models.IntegerField()
+    name = models.CharField(max_length=155)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    location = models.CharField(max_length=255)
+    foster_parents = ArrayField(models.CharField(max_length=200), blank=True)
+    foster_siblings = ArrayField(models.CharField(max_length=200), blank=True)
+    notes = ArrayField(models.CharField(max_length=500), blank=True)
+    time_stamp = models.DateTimeField(auto_now_add=True)
