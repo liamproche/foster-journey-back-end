1ba2810636b2723596e0e39ec53c912acb2b85ce
Liam Roche <roche.liamp@gmail.com>
2022-04-19 16:28:23 -0400

    minor touchups

--- backend_rest_api/placements_api/views.py
+++ backend_rest_api/placements_api/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import PlacementSerializer
 from .serializers import ParentSerializer
-from .models import Placement
-from .models import FosterParent
+from .models import Placement, FosterParent
 
 # Create your views here.
 class PlacementList(generics.ListCreateAPIView):
     queryset = Placement.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
     serializer_class = PlacementSerializer # tell django what serializer to use
 
+# CREATES PARENT LIST
 class ParentList(generics.ListCreateAPIView):
     queryset = FosterParent.objects.all().order_by('id')
     serializer_class = ParentSerializer
+
 
 # POSSIBLY NOT NEEDED - DELETE AFTER REFACTOR FINISHED
 class PlacementDetail(generics.RetrieveUpdateDestroyAPIView):
