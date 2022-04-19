45735811c707aee45571800c8e03f0b3b82c2a38
Liam Roche <roche.liamp@gmail.com>
2022-04-19 14:07:59 -0400

    added parent serializer

--- backend_rest_api/placements_api/serializers.py
+++ backend_rest_api/placements_api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers 
 from .models import Placement 
+from .models import FosterParent
 
 class PlacementSerializer(serializers.ModelSerializer): # serializers.ModelSerializer just tells django to convert sql to JSON
     class Meta:
         model = Placement # tell django which model to use
         # THIS TELLS DJANGO WHAT DB FIELDS TO GRAB AND SEND TO THE FRONT END!!!!!(6-HOURS OF MY LIFE)
         fields = ('id', 'num', 'name', 'start_date', 'end_date', 'location', 'notes', 'time_stamp', 'user')
+
+class ParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FosterParent 
+        fields = ('first_name', 'last_name', 'url', 'time_stamp', 'placement')
