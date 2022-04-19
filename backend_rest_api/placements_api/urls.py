344e3909564747b234f2bc2411bbcf9896f04650
Liam Roche <roche.liamp@gmail.com>
2022-04-19 14:01:50 -0400

    updated placement serializer

--- backend_rest_api/placements_api/urls.py
+++ backend_rest_api/placements_api/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    # PLACEMENTS
     path('api/placements', views.PlacementList.as_view(), name='placement_list'), # api/placements will be routed to the PlacementList view for handling
+    
+    # NOT YET NEEDED - LEAVE IN FOR NOW INCASE REFACTORING REQUIRES A DETAIL PATH
     path('api/placements/<int:pk>', views.PlacementDetail.as_view(), name='placement_detail'), # api/placements will be routed to the PlacementDetail view for handling
+
 ]
