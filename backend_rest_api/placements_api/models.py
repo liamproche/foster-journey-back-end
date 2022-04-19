40459a3308bddd99e9fe0e478b13b10ad2dd2b9a
Liam Roche <roche.liamp@gmail.com>
2022-04-19 12:35:43 -0400

    added seperate models for foster parents and foster siblings

--- backend_rest_api/placements_api/models.py
+++ backend_rest_api/placements_api/models.py
@@ -1,6 +1,6 @@
+from tkinter import CASCADE
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
-from django.contrib.auth.models import User
 from backend_rest_api import settings
 
 # Create your models here.
@@ -15,3 +15,14 @@ class Placement(models.Model):
     notes = ArrayField(models.CharField(max_length=500, null=True), blank=True, default=list)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True)
     time_stamp = models.DateTimeField(auto_now_add=True)
+
+class FosterParent(models.Model):
+    first_name = models.CharField(max_length=55, null=False)
+    last_name = models.CharField(max_length=55, null=True)
+    url = models.URLField(max_length=255)
+    placement = models.ForeignKey(Placement, default=None, on_delete=models.CASCADE)
+
+class FosterSibling(models.Model):
+    first_name = models.CharField(max_length=55, null=False)
+    last_name = models.CharField(max_length=55, null=True)
+    placement = models.ForeignKey(Placement, default=None, on_delete=models.CASCADE)
