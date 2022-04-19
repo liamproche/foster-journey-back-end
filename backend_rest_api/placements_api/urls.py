5372cb06ea3d7c79149c4fcc9e27a9d96c09bdc6
Liam Roche <roche.liamp@gmail.com>
2022-04-19 14:14:29 -0400

    created parent route and serializer

--- backend_rest_api/placements_api/urls.py
+++ backend_rest_api/placements_api/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    # PARENTS
+    path('api/parents', views.ParentList.as_view(), name='parent_list'),
+
     # PLACEMENTS
     path('api/placements', views.PlacementList.as_view(), name='placement_list'), # api/placements will be routed to the PlacementList view for handling
     
--- backend_rest_api/placements_api/views.py
+++ backend_rest_api/placements_api/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import PlacementSerializer
+from .serializers import ParentSerializer
 from .models import Placement
+from .models import FosterParent
 
 # Create your views here.
 class PlacementList(generics.ListCreateAPIView):
     queryset = Placement.objects.all().order_by('id') # tell django how to retrieve all objects from the DB, order by id ascending
     serializer_class = PlacementSerializer # tell django what serializer to use
 
+class ParentList(generics.ListCreateAPIView):
+    queryset = FosterParent.objects.all().order_by('id')
+    serializer_class = ParentSerializer
 
 # POSSIBLY NOT NEEDED - DELETE AFTER REFACTOR FINISHED
 class PlacementDetail(generics.RetrieveUpdateDestroyAPIView):
